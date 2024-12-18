bd2ddeaa8e125a638a1c2d5fcd394f0087d5c66b
Your Name <you@example.com>
2024-12-18 12:33:40 +0900

    first commit

---
+++ db_lesson.sql
@@ -0,0 +1,6 @@
+CREATE TABLE departments (
+  department_id INT unsigned auto_increment PRIMARY KEY,
+  name VARCHAR(20) NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+   );
